37f2e429bc9cd85dc9f0fcd49a28a4bd88184e68
Anoop M D <anoop.md@sinilabs.co.in>
2018-05-18 02:13:30 +0530

    Simple Component

---
+++ src/jet-composites/simple-alert/simple-alert-viewModel.js
@@ -0,0 +1,44 @@
+'use strict';
+define(
+    ['ojs/ojcore', 'knockout', 'jquery'], function (oj, ko, $) {
+
+    function SimpleAlertComponentModel(context) {
+        var self = this;
+        
+        //At the start of your viewModel constructor
+        var busyContext = oj.Context.getContext(context.element).getBusyContext();
+        var options = {"description": "CCA Startup - Waiting for data"};
+        self.busyResolve = busyContext.addBusyState(options);
+
+        self.composite = context.element;
+
+        //Example observable
+        self.messageText = ko.observable('Hello from Example Component');
+
+        // Example for parsing context properties
+        // if (context.properties.name) {
+        //     parse the context properties here
+        // }
+
+        //Once all startup and async activities have finished, relocate if there are any async activities
+        self.busyResolve();
+    };
+    
+    //Lifecycle methods - uncomment and implement if necessary 
+    //ExampleComponentModel.prototype.activated = function(context){
+    //};
+
+    //ExampleComponentModel.prototype.attached = function(context){
+    //};
+
+    //ExampleComponentModel.prototype.bindingsApplied = function(context){
+    //};
+
+    //ExampleComponentModel.prototype.detached = function(context){
+    //};
+
+    //ExampleComponentModel.prototype.propertyChanged = function(context){
+    //};
+
+    return SimpleAlertComponentModel;
+});
